ec6fcc884cc3930ea3d316af50af78bf547036c6
Victor Cordova <victor.cordova@travelperk.com>
2020-05-15 19:57:51 +0200

    Make ingredients mandatory
    This condition has been added because it doesn't make sense to have a recipe with no ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,22 +11,19 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=True)
 
     class Meta:
         model = Recipe
         fields = ('name', 'description', 'ingredients')
 
     def create(self, validated_data):
-        ingredients = None
-        if 'ingredients' in validated_data:
-            ingredients = validated_data.pop('ingredients', None)
+        ingredients = validated_data.pop('ingredients', None)
 
         recipe = Recipe.objects.create(**validated_data)
 
-        if ingredients:
-            for ingredient in ingredients:
-                Ingredient.objects.create(recipe=recipe, **ingredient)
+        for ingredient in ingredients:
+            Ingredient.objects.create(recipe=recipe, **ingredient)
 
         return recipe
 

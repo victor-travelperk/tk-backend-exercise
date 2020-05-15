8fd9f3362e2663003522d0dc9ca85b9469a7b68e
Victor Cordova <victor.cordova@travelperk.com>
2020-05-15 14:14:23 +0200

    Add Recipe and Ingredient models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
